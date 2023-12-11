6849e05cc0b47d8046a9f25cbab52c075bd99acd
johnson-oragui <johnson-oragui@gmail.com>
2023-12-11 11:35:11 +0100

    created new dir and moved the home route to the route/main dir

--- app.js
+++ app.js
@@ -1,20 +1,38 @@
 require('dotenv').config();
 import express from 'express';
-import expressEjsLayouts from 'express-ejs-layouts'; 
+import expressEjsLayouts from 'express-ejs-layouts';
+
+import router from './server/routes/main';
+// ##########################################################
 
 // instance of express
 const app = express();
 
+// ###########################################################
+
 // middle ware
 app.use(expressEjsLayouts);
+app.use(express.static('static'));
 
+// ##############################################################
+
+// Templates ENgine
+
+// specifies the path to the default layout file to be used when rendering views
+// It defines the overall structure of html pages
+app.set('layout', './layouts/main');
+// specify the template engine to be used
+app.set('view engine', 'ejs');
+
+// #################################################################
 // port
 const PORT = process.env.PORT || 5000;
 
+// ################################################################
 // get methods
-app.get('/', (req, res) => {
-  res.send('hello world');
-});
+app.get('/', router);
+
+// ###############################################################
 
 // event listener
 app.listen(PORT, () => {
