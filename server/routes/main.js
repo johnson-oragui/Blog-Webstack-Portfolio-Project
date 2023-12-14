de4dbb5e01a6b4fa728b4c4c3be5801c7d9485a6
johnson-oragui <johnson-oragui@gmail.com>
2023-12-14 17:11:35 +0100

    removed imports of error controllers

--- server/routes/main.js
+++ server/routes/main.js
@@ -6,12 +6,6 @@ import {
   getPost,
 } from '../controllers/mainController';
 
-import {
-  getNotFound,
-  getServerError,
-  getUnauthorized,
-} from '../controllers/errorControllers';
-
 const router = express.Router();
 
 router.get('/', getHomePage);
@@ -21,11 +15,5 @@ router.get('/about', getAboutPage);
 router.get('/contact', getContactPage);
 
 router.get('/post/:id', getPost);
-// Error routes
-router.get('/notfound', getNotFound);
-
-router.get('/serverError', getServerError);
-
-router.get('/unauthorized', getUnauthorized);
 
 export default router;
