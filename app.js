c062fef4dc683f9aba0741dbd4b174b91594fb0f
johnson-oragui <johnson-oragui@gmail.com>
2023-12-16 13:53:57 +0100

    moved error middleware to a new file inside server folder

--- app.js
+++ app.js
@@ -7,8 +7,8 @@ import mainRouter from './server/routes/main';
 import errorRouter from './server/routes/error';
 import adminRouter from './server/routes/admin';
 import dbConnect from './utils/db';
-// import error classes from custom file
-import { NotFoundError, UnauthorizedError } from './server/errorClasses';
+// import error middlewar
+import errorMiddleware from './server/errorMiddleware';
 
 const rotatingFileStream = require('rotating-file-stream');
 
@@ -60,34 +60,8 @@ app.use('/', errorRouter);
 // get methods for admin pages
 app.use('/', adminRouter);
 
-// middlewar foe error handling
-
-app.use((err, req, res, next) => {
-  console.error(err.stack);
-
-  // Check the type of error and render the appropriate EJS page
-  if (err instanceof SyntaxError && err.status === 400 && 'body' in err) {
-    // Handle JSON parse error
-    res.status(400).render('error400', { locals: { title: '400 Bad Request', description: 'Invalid JSON' } });
-  } else if (err instanceof SyntaxError && err.status === 404 && 'body' in err) {
-    // Handle JSON parse error
-    res.status(404).render('error404', { locals: { title: '404 Not Found', description: 'Page not found' } });
-  } else if (err instanceof NotFoundError) {
-    res.status(404).render('error404', { locals: { title: '404 Not Found', description: 'Page not found' } });
-  } else if (err instanceof UnauthorizedError) {
-    res.status(401).render('error401', { locals: { title: '401 Unauthorized', description: 'Unauthorized access' } });
-  } else {
-    // Default error handling
-    res.status(err.statusCode || 500).render('error500', {
-      locals: {
-        title: `${err.statusCode || 500} Internal Server Error`,
-        description: 'Internal server error',
-      },
-    });
-  }
-  // Call next without any conditions to ensure execution of subsequent middleware
-  next();
-});
+// middlewar for error handling
+app.use(errorMiddleware);
 
 // event listener
 app.listen(PORT, () => {
