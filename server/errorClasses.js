7a85fb216a568151c4351ba2217919216bb96434
johnson-oragui <johnson-oragui@gmail.com>
2023-12-16 13:35:09 +0100

    moved erroClasses.js to server folder

---
+++ server/errorClasses.js
@@ -0,0 +1,15 @@
+export class NotFoundError extends Error {
+  constructor(message) {
+    super(message);
+    this.name = 'NotFoundError';
+    this.statusCode = 404;
+  }
+}
+
+export class UnauthorizedError extends Error {
+  constructor(message) {
+    super(message);
+    this.name = 'UnauthorizedError';
+    this.statusCode = 401;
+  }
+}
