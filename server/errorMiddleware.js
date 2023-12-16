44fede1a0e7f55737f3cd2bfa7783b1c68a8c3b7
johnson-oragui <johnson-oragui@gmail.com>
2023-12-16 14:58:16 +0100

    modified to use the right paths, and redirect

--- server/errorMiddleware.js
+++ server/errorMiddleware.js
@@ -6,22 +6,17 @@ const errorMiddleware = ((err, req, res, next) => {
   // Check the type of error and render the appropriate EJS page
   if (err instanceof SyntaxError && err.status === 400 && 'body' in err) {
     // Handle JSON parse error
-    res.status(400).render('error400', { locals: { title: '400 Bad Request', description: 'Invalid JSON' } });
+    res.render(400, '/notfound');
   } else if (err instanceof SyntaxError && err.status === 404 && 'body' in err) {
     // Handle JSON parse error
-    res.status(404).render('error404', { locals: { title: '404 Not Found', description: 'Page not found' } });
+    res.redirect(404, '/notfound');
   } else if (err instanceof NotFoundError) {
-    res.status(404).render('error404', { locals: { title: '404 Not Found', description: 'Page not found' } });
+    res.redirect(404, '/notfound');
   } else if (err instanceof UnauthorizedError) {
-    res.status(401).render('error401', { locals: { title: '401 Unauthorized', description: 'Unauthorized access' } });
+    res.redirect(401, '/unauthorized');
   } else {
     // Default error handling
-    res.status(err.statusCode || 500).render('error500', {
-      locals: {
-        title: `${err.statusCode || 500} Internal Server Error`,
-        description: 'Internal server error',
-      },
-    });
+    res.redirect(500, '/serverError');
   }
   // Call next without any conditions to ensure execution of subsequent middleware
   next();
