54d126ebe1ac9a3793f24a8453c9478519896515
johnson-oragui <johnson-oragui@gmail.com>
2023-12-17 15:58:36 +0100

    returned all pages

--- server/errorMiddleware.js
+++ server/errorMiddleware.js
@@ -6,17 +6,17 @@ const errorMiddleware = ((err, req, res, next) => {
   // Check the type of error and render the appropriate EJS page
   if (err instanceof SyntaxError && err.status === 400 && 'body' in err) {
     // Handle JSON parse error
-    res.render('error400');
+    return res.render('error400');
   } else if (err instanceof SyntaxError && err.status === 404 && 'body' in err) {
     // Handle JSON parse error
-    res.redirect('/notfound');
+    return res.redirect('/notfound');
   } else if (err instanceof NotFoundError || err.status === 404) {
-    res.redirect('/notfound');
+    return res.redirect('/notfound');
   } else if (err instanceof UnauthorizedError || err.status === 401) {
-    res.redirect('/unauthorized');
+    return res.redirect('/unauthorized');
   } else {
     // Default error handling
-    res.redirect('/serverError');
+    return res.redirect('/serverError');
   }
   // Call next without any conditions to ensure execution of subsequent middleware
   next();
