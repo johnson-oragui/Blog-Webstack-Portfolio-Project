f29b8520550dc1b1fbe6e18072ab896b7a6fa0c2
johnson-oragui <johnson-oragui@gmail.com>
2023-12-19 15:20:58 +0100

    protected admin routes with authmiddleware

--- server/routes/admin.js
+++ server/routes/admin.js
@@ -3,15 +3,26 @@ import {
   getLoginPage,
   getRegPage,
   postLoginPage,
+  getDashboard,
+  getNotes,
+  logout,
 } from '../controllers/adminControllers';
 import postRegPage from '../controllers/registrationController';
+import authenticationMiddleware from './authMiddleware/authMiddleware';
+import refreshTokenRouter from './refreshTokenRouter';
 
 const router = express.Router();
 
 router.get('/login', getLoginPage);
 router.get('/register', getRegPage);
+router.get('/logout', logout);
+
+router.get('/dashboard', authenticationMiddleware, getDashboard);
+router.get('/notes', authenticationMiddleware, getNotes);
 
 router.post('/register', postRegPage);
 router.post('/login', postLoginPage);
+
+router.use(refreshTokenRouter);
 
 export default router;
