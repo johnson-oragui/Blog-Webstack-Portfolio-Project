981a566ccb2b78fa87c936793f6332f3e92806d5
johnson-oragui <johnson-oragui@gmail.com>
2023-12-21 16:51:58 +0100

    added routes or getAddPost, postAddPost, getNotes

--- server/controllers/adminControllers.js
+++ server/controllers/adminControllers.js
@@ -1,10 +1,7 @@
-import jwt from 'jsonwebtoken';
 import Post from '../models/post';
 import User from '../models/user';
 import { checkHashedPwd } from '../utils/bcryptUtils';
-import cookieParser from 'cookie-parser';
 import { generateAcessToken } from '../routes/authMiddleware/generateVerifyAccessToken';
-import authenticationMiddleware from '../routes/authMiddleware/authMiddleware';
 import { generateRefreshToken } from '../routes/authMiddleware/generateFreshToken';
 
 const adminLayout = '../views/layouts/admin';
@@ -29,12 +26,12 @@ export const getLoginPage = async (req, res, next) => {
       layout: adminLayout,
       message,
       messageClass: 'success',
-      username: req.params.username || null,
+      username: null,
       password: null,
       userToken,
     });
   } catch (error) {
-    console.error('error in getAdminPage method', error.message);
+    console.error('error in getLoginPage method', error.message);
     next(error);
   }
 };
@@ -118,7 +115,6 @@ export const postLoginPage = async (req, res, next) => {
 
       res.cookie('token', token, { httpOnly: true });
       res.cookie('refreshToken', refreshToken, { httpOnly: true });
-      // return res.render('admin/dashboard', { layout: adminLayout, userToken }); // index.ejs inside admin folder
       return res.redirect('/dashboard');
     }
   } catch (error) {
@@ -166,11 +162,127 @@ export const getDashboard = async (req, res, next) => {
     const userToken = req.cookies.token;
     console.log('userToken from geDashboard route: ', userToken);
 
-    return res.render('admin/dashboard', { locals, userToken, layout: adminLayout });
+    const data = await Post.find();
+
+    return res.render('admin/dashboard', {
+      locals,
+      userToken,
+      layout: adminLayout,
+      data,
+    });
   } catch (error) {
     console.error('Error in getDashboard method');
     next(error);
   }
+};
+
+// get add post controller
+export const getAddPost = async (req, res, next) => {
+  try {
+    if (req.method === 'GET') {
+      const locals = {
+        title: 'Add post Page',
+        description: 'Add post',
+      };
+      console.log('userToken from getAddPost route: ', req.cookies.token);
+
+      return res.render('admin/add-post', {
+        locals,
+        userToken: req.cookies.token,
+        layout: adminLayout,
+        message: 'Fill in all to create a post, category is optional',
+        messageClass: 'success',
+        title: null,
+        category: null,
+        body: null,
+      });
+    }
+  } catch (error) {
+    console.error('Error in getAddPost method');
+    next(error);
+  }
+};
+
+// post add post controller
+export const postAddPost = async (req, res, next) => {
+  try {
+    if (req.method === 'POST') {
+      const locals = {
+        title: 'Add post Page',
+        description: 'Add post',
+      };
+      const userToken = req.cookies.token;
+      console.log('userToken from postAddPost route: ', userToken);
+
+      const { title, category, body } = req.body;
+
+      console.log('title section,  check for title');
+      if (!title || title.trim() === '') {
+        console.log('title missing');
+        return res.render('admin/add-post', {
+          locals,
+          userToken,
+          title,
+          category,
+          body,
+          message: 'Title missing',
+          messageClass: 'failure',
+        });
+      }
+      console.log('title section,  check for title passed');
+
+      console.log('body section,  check for body');
+      if (!body || body.trim() === '') {
+        console.log('body missing');
+        return res.render('admin/add-post', {
+          locals,
+          userToken,
+          title,
+          category,
+          body,
+          message: 'Content missing',
+          messageClass: 'failure',
+        });
+      }
+      console.log('body section,  check for body passed');
+
+      const dataToInsert = {
+        title,
+        category,
+        body,
+      };
+
+      const newPost = new Post(dataToInsert);
+
+      const data = await Post.create(newPost);
+      console.log('successfully inserted: ', data._id);
+
+      return res.redirect('/dashboard');
+    }
+  } catch (error) {
+    console.error('Error in postAddPost method');
+    next(error);
+  }
+};
+
+// get edit post controller
+export const getEditPost = async (req, res, next) => {
+
+};
+
+// post edit post controller
+export const postAditPost = async (req, res, next) => {
+
+};
+
+// get delete post controller
+export const getDeletePost = async (req, res, next) => {
+
+};
+
+// post delete post controller
+export const postDeletePost = async (req, res, next) => {
+
 };
 
 export const getNotes = async (req, res, next) => {
