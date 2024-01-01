7291c0abafe3b3323b368967f7027aa08d1f8d1e
johnson-oragui <johnson-oragui@gmail.com>
2024-01-01 19:09:42 +0100

    addd schema for replies

--- server/models/comments.js
+++ server/models/comments.js
@@ -1,5 +1,23 @@
 import mongoose from 'mongoose';
 
+const ReplySchema = mongoose.Schema({
+  // Author of thecomment
+  author: {
+    type: String,
+    required: true,
+  },
+  // Content/Body of the comment
+  content: {
+    type: String,
+    required: true,
+  },
+  // TimeStamp indicating when the comment was created
+  createAt: {
+    type: Date,
+    default: Date.now, // Default to the current date and time
+  },
+});
+
 const CommentSchema = mongoose.Schema({
   // Author of thecomment
   author: {
@@ -16,6 +34,12 @@ const CommentSchema = mongoose.Schema({
     type: Date,
     default: Date.now, // Default to the current date and time
   },
+  replies: [ // for nested replies
+    {
+      type: ReplySchema,
+      required: false,
+    },
+  ],
 });
 
 export default CommentSchema;
