c8859329eb200dd19067dec144a20ef07917a878
johnson-oragui <johnson-oragui@gmail.com>
2024-01-03 12:59:52 +0100

    modified to seperate middleware from redis router

--- server/routes/redisRouteHandler.js
+++ server/routes/redisRouteHandler.js
@@ -1,7 +1,11 @@
 /* eslint-disable import/no-extraneous-dependencies */
 import express from 'express';
+import cacheMiddleware from '../middleWare/cacheMiddleware';
 import { getHomePage } from '../controllers/mainController';
 
 const redisRouter = express.Router();
+
+// apply caching middleware to the route
+redisRouter.get('/', cacheMiddleware, getHomePage);
 
 export default redisRouter;
